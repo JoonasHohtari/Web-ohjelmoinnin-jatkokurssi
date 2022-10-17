b6a4bc9daaeb7e801fd780dba58b93c189883530
JoonasHohtari <zzounass@gmail.com>
2022-10-17 13:50:27 +0300

    Didn't find a solution yet

--- exer8/src/App.js
+++ exer8/src/App.js
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import Counter from "./components/Counter";
-
+// KESKEN
 function App() {
-  const clickHandler = () => {};
-
+  /*
+    const incrementValue = () => {
+      setValue1(value2++);
+      console.log(value2);
+    };
+*/
   const [value1, setValue1] = useState(0);
   const [value2, setValue2] = useState(0);
 
--- exer8/src/components/Counter.js
+++ exer8/src/components/Counter.js
@@ -1,10 +1,23 @@
 const Counter = (props) => {
+  let count = 0;
+  const incrementValue = () => {
+    if (props.setValue2) {
+      count++;
+      props.value1++;
+    } else {
+      count++;
+      props.value2++;
+    }
+  };
   return (
     <div>
       <p>
-        <button onClick>+</button>
+        <button onClick={incrementValue}></button>
       </p>
     </div>
   );
 };
+
 export default Counter;
+
+// En tiedä miten jatkaa
